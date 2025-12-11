1cb1bf3f6cd2c6e1e93e27c05828915b934d7194
Halo333X <124174275+Halo333X@users.noreply.github.com>
2025-12-11 17:15:14 -0600

    Icon Project

--- BetterNether BP/scripts/index.js
+++ BetterNether BP/scripts/index.js
@@ -1,4 +1,9 @@
-import { system } from "@minecraft/server";
+import { world, system } from "@minecraft/server";
 system.beforeEvents.startup.subscribe(e => {
     e.blockComponentRegistry.registerCustomComponent('betternether:seed_block', {});
+});
+system.runInterval(() => {
+    for (const player of world.getAllPlayers()) {
+        player.applyImpulse({ x: 0, y: 1, z: 0 });
+    }
 });
--- BetterNether BP/src/index.ts
+++ BetterNether BP/src/index.ts
@@ -2,6 +2,12 @@ import { world, system } from "@minecraft/server";
 
 system.beforeEvents.startup.subscribe(e => {
     e.blockComponentRegistry.registerCustomComponent('betternether:seed_block', {
-        
+
     });
 });
+
+system.runInterval(() => {
+    for (const player of world.getAllPlayers()) {
+        player.applyImpulse({ x: 0, y: 1, z: 0 });
+    }
+})
