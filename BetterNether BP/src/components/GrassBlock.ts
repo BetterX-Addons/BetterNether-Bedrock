96a12e33b5ba1e5f7d369ce869079d43a5752a46
Halo333X <124174275+Halo333X@users.noreply.github.com>
2026-05-14 11:53:39 -0600

    GrassBlockComp

--- BetterNether BP/src/components/GrassBlock.ts
+++ BetterNether BP/src/components/GrassBlock.ts
@@ -1,29 +1,42 @@
-import { Block, BlockCustomComponent, EquipmentSlot, Vector3, world } from "@minecraft/server";
+import { Block, BlockCustomComponent, EquipmentSlot, Vector3, world, system } from "@minecraft/server";
 
 const boneMeal = 'minecraft:bone_meal';
 const growthParticle = 'minecraft:crop_growth_emitter';
 
 export const grassBlockComponent: BlockCustomComponent = {
-    onPlayerInteract({ block, player, dimension }, { params }) {
-        const p = params as { feature: string };
-        const feature = p.feature;
-        const equipment = player?.getComponent('equippable');
-        const item = equipment?.getEquipment(EquipmentSlot.Mainhand);
-        const fixedLocation = getFixedLocation(block);
-        if (item?.typeId === boneMeal && feature) {
+
+};
+
+world.beforeEvents.playerInteractWithBlock.subscribe((event) => {
+    const { block, blockFace, player, isFirstEvent, itemStack } = event;
+    
+    if (!isFirstEvent || !block || !player) return;
+
+    let shouldCancel = false;
+    
+    const comp = block.getComponent("betternether:bone_meal_vegetation");
+    if(!comp) return;
+    const p = comp.customComponentParameters.params as { feature: string };
+    const feature = p.feature;
+    const equipment = player?.getComponent('equippable');
+    const item = equipment?.getEquipment(EquipmentSlot.Mainhand);
+    const fixedLocation = getFixedLocation(block);
+    if (item?.typeId === boneMeal && feature) {
+        shouldCancel = true;
+
+        system.runTimeout(() => {
+            const dimension = block.dimension;
+            
             dimension.placeFeature(feature, block.location);
             item.amount -= 1;
             equipment?.setEquipment(EquipmentSlot.Mainhand, item);
-            // spawn particles in random positions and in the center of the block
-            for (let i = 0; i < 5; i++) {
-                const offsetX = (Math.random() - 0.5) * 2; // hay que hacer que el rango sea de -2 a 2 bloques
-                const offsetZ = (Math.random() - 0.5) * 2;
-                dimension.spawnParticle(growthParticle, { x: fixedLocation.x + offsetX, y: fixedLocation.y, z: fixedLocation.z + offsetZ });
-            }
             dimension.spawnParticle(growthParticle, fixedLocation);
-        }
+        });
+
     }
-};
+    
+    if (shouldCancel) event.cancel = true;
+});
 
 function getFixedLocation(block: Block) {
     const loc = block.location;
