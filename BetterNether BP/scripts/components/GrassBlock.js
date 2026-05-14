ac81af1c0a205a3c5ae5f7be6d695e6797f83607
Halo333X <124174275+Halo333X@users.noreply.github.com>
2026-05-13 20:11:20 -0600

    System Removal on GrassBlock.ts

--- BetterNether BP/scripts/components/GrassBlock.js
+++ BetterNether BP/scripts/components/GrassBlock.js
@@ -1,39 +1,27 @@
-import { EquipmentSlot, world, system } from "@minecraft/server";
+import { EquipmentSlot } from "@minecraft/server";
 const boneMeal = 'minecraft:bone_meal';
 const growthParticle = 'minecraft:crop_growth_emitter';
-export const grassBlockComponent = {};
-// Prevent swing animation on custom blocks without using bone meal.
-world.beforeEvents.playerInteractWithBlock.subscribe((event) => {
-    const { block, player, isFirstEvent } = event;
-    if (!isFirstEvent || !block || !player)
-        return;
-    let shouldCancel = false;
-    const comp = block.getComponent("betternether:bone_meal_vegetation");
-    if (!comp)
-        return;
-    const p = comp.customComponentParameters.params;
-    const feature = p.feature;
-    const equipment = player?.getComponent('equippable');
-    const item = equipment?.getEquipment(EquipmentSlot.Mainhand);
-    const fixedLocation = getFixedLocation(block);
-    if (item?.typeId === boneMeal && feature) {
-        shouldCancel = true;
-        system.runTimeout(() => {
-            const dimension = block.dimension;
+export const grassBlockComponent = {
+    onPlayerInteract({ block, player, dimension }, { params }) {
+        const p = params;
+        const feature = p.feature;
+        const equipment = player?.getComponent('equippable');
+        const item = equipment?.getEquipment(EquipmentSlot.Mainhand);
+        const fixedLocation = getFixedLocation(block);
+        if (item?.typeId === boneMeal && feature) {
             dimension.placeFeature(feature, block.location);
             item.amount -= 1;
             equipment?.setEquipment(EquipmentSlot.Mainhand, item);
+            // spawn particles in random positions and in the center of the block
             for (let i = 0; i < 5; i++) {
-                const offsetX = (Math.random() - 0.5) * 2;
-                const offsetZ = (Math.random() - 0.5) * 2;
+                const offsetX = (Math.random() - 0.5) * 0.5;
+                const offsetZ = (Math.random() - 0.5) * 0.5;
                 dimension.spawnParticle(growthParticle, { x: fixedLocation.x + offsetX, y: fixedLocation.y + 0.5, z: fixedLocation.z + offsetZ });
             }
             dimension.spawnParticle(growthParticle, fixedLocation);
-        });
+        }
     }
-    if (shouldCancel)
-        event.cancel = true;
-});
+};
 function getFixedLocation(block) {
     const loc = block.location;
     return { x: loc.x + 0.5, y: loc.y, z: loc.z + 0.5 };
--- BetterNether BP/src/components/GrassBlock.ts
+++ BetterNether BP/src/components/GrassBlock.ts
@@ -1,48 +1,29 @@
-import { Block, BlockCustomComponent, EquipmentSlot, Vector3, world, system } from "@minecraft/server";
+import { Block, BlockCustomComponent, EquipmentSlot, Vector3, world } from "@minecraft/server";
 
 const boneMeal = 'minecraft:bone_meal';
 const growthParticle = 'minecraft:crop_growth_emitter';
 
 export const grassBlockComponent: BlockCustomComponent = {
-
-};
-
-// Prevent swing animation on custom blocks without using bone meal.
-world.beforeEvents.playerInteractWithBlock.subscribe((event) => {
-    const { block, player, isFirstEvent } = event;
-    
-    if (!isFirstEvent || !block || !player) return;
-
-    let shouldCancel = false;
-    
-    const comp = block.getComponent("betternether:bone_meal_vegetation");
-    if(!comp) return;
-    const p = comp.customComponentParameters.params as { feature: string };
-
-    const feature = p.feature;
-    const equipment = player?.getComponent('equippable');
-    const item = equipment?.getEquipment(EquipmentSlot.Mainhand);
-    const fixedLocation = getFixedLocation(block);
-    if (item?.typeId === boneMeal && feature) {
-        shouldCancel = true;
-
-        system.runTimeout(() => {
-            const dimension = block.dimension;
+    onPlayerInteract({ block, player, dimension }, { params }) {
+        const p = params as { feature: string };
+        const feature = p.feature;
+        const equipment = player?.getComponent('equippable');
+        const item = equipment?.getEquipment(EquipmentSlot.Mainhand);
+        const fixedLocation = getFixedLocation(block);
+        if (item?.typeId === boneMeal && feature) {
             dimension.placeFeature(feature, block.location);
             item.amount -= 1;
             equipment?.setEquipment(EquipmentSlot.Mainhand, item);
+            // spawn particles in random positions and in the center of the block
             for (let i = 0; i < 5; i++) {
-                const offsetX = (Math.random() - 0.5) * 2;
-                const offsetZ = (Math.random() - 0.5) * 2;
+                const offsetX = (Math.random() - 0.5) * 0.5;
+                const offsetZ = (Math.random() - 0.5) * 0.5;
                 dimension.spawnParticle(growthParticle, { x: fixedLocation.x + offsetX, y: fixedLocation.y + 0.5, z: fixedLocation.z + offsetZ });
             }
             dimension.spawnParticle(growthParticle, fixedLocation);
-        });
-
+        }
     }
-    
-    if (shouldCancel) event.cancel = true;
-});
+};
 
 function getFixedLocation(block: Block) {
     const loc = block.location;
